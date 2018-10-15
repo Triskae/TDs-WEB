3c1aac32d76e93ae6e5a52c39a0b5d3a2a4e67eb
Filipe Doutel Silva <filipe.doutelsilva@hotmail.fr>
2018-10-15 14:55:47 +0200

    Ajout des nouveautés faites le 15-octobre

---
+++ TP2/src/preferences.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <meta charset="utf-8"/>
+
+    <?php
+    session_start();
+    // Insertion de la balise link
+    if (isset($_POST['style'])) {
+        if ($_POST['style'] == 'Vert et Jaune') {
+            $_SESSION['style'] = "<link rel='stylesheet' href='vert et jaune.css' type='text/css' />";
+            $_SESSION["select1"] = "selected";
+            $_SESSION["select2"] = "";
+            $_SESSION["select3"] = "";
+        } elseif ($_POST['style'] == 'Bleu') {
+            $_SESSION['style'] = "<link rel='stylesheet' href='blue.css' type='text/css' />";
+            $_SESSION["select1"] = "";
+            $_SESSION["select2"] = "selected";
+            $_SESSION["select3"] = "";
+        } else {
+            $_SESSION['style'] = "<link rel='stylesheet' href='italic.css' type='text/css' />";
+            $_SESSION["select1"] = "";
+            $_SESSION["select2"] = "";
+            $_SESSION["select3"] = "selected";
+        }
+    }
+    echo $_SESSION['style'];
+    ?>
+
+    <title>TP2 - Web</title>
+</head>
+
+<body>
+
+<form name="saisiePreference" method="post" action="preferences.php">
+    Choississez votre feuille de style : <br/>
+    <select name="style" size="3">
+        <option <?php echo $_SESSION["select1"] ?>>Vert et Jaune
+        <option <?php echo $_SESSION["select2"] ?>>Bleu
+        <option <?php echo $_SESSION["select3"] ?>>Italique
+    </select>
+    <input type="submit" name="valider" value="Valider"/><br/>
+</form>
+
+</body>
+
+</html>
--- TP2/src/tableau.php
+++ TP2/src/tableau.php
@@ -7,12 +7,12 @@
  */
 
 
-$tab =  array (
+$tab = array(
     array("date.php", "23-09-2016", "1856"),
     array("tableau.php", "223-09-2016", "1875")
 );
 
-$titre = array ("Nom du fichier", "Date de modification","Taille");
+$titre = array("Nom du fichier", "Date de modification", "Taille");
 
 
 echo "<pre>";
@@ -28,8 +28,12 @@ echo "</pre>";
 
 echo genereTableau($titre, $tab);
 
-function genereTableau ($title, $body) {
-    $HTMLstring = "<table border='1px solid black'><tr>";
+session_start();
+echo $_SESSION['style'];
+
+function genereTableau($title, $body)
+{
+    $HTMLstring = "<table border=\"1\" solid black><caption>Tableau des fichiers V2</caption>";
     if ($title != null && $body != null) {
         foreach ($title as $element) {
             $HTMLstring .= "<th>";
@@ -55,13 +59,62 @@ function genereTableau ($title, $body) {
     return $HTMLstring;
 }
 
-$local = glob("*");
-foreach ($local as $file) {
-    if (is_file($file)) {
-        $filesize = filesize($file);
-    } else $filesize = "dir";
-    $filedate = filemtime($file);
-    $date = date("d F Y", $filedate);
-    echo "$file date de $date et pese $filesize";
-    echo "<br>";
+function genereTableauv2($title, $body)
+{
+    if (isset($_GET['tri'])) {
+        if ($_GET['tri'] == 'reverse') {
+            rsort($body, SORT_REGULAR);
+        } elseif ($_GET['tri'] == 'normal') {
+            sort($body, SORT_REGULAR);
+        }
+    }
+    $HTMLstring = '<table border="1" solid black><caption>Tableau des fichiers V2</caption>';
+    $HTMLstring .= '<tr>';
+
+    foreach ($title as $key => $colonne) {
+        $HTMLstring .= '<th>';
+        $HTMLstring .= $colonne;
+        $HTMLstring .= '</th>';
+    }
+    $HTMLstring .= '</tr>';
+    $HTMLstring .= '<tr>';
+
+    foreach ($body as $line) {
+        $HTMLstring .= '<tr>';
+        if (count($line) == count($title)) {
+            $HTMLstring .= '<td>';
+            $HTMLstring .= "<a href='$line[0]'> $line[0]</a>";
+            $HTMLstring .= '</td>';
+        } else $HTMLstring .= '';
+        for ($i = 1; $i < count($line); $i++) {
+            if (count($line) == count($title)) {
+                $HTMLstring .= '<td>';
+                $HTMLstring .= $line[$i];
+                $HTMLstring .= '</td>';
+            } else $HTMLstring .= '';
+        }
+        $HTMLstring .= '</tr>';
+    }
+    $HTMLstring .= '</table>';
+    return $HTMLstring;
 }
+
+$local = glob("*");
+$newArray = array();
+
+foreach ($local as $filename) {
+    if (is_file($filename)) {
+        $filesize = filesize($filename);
+    } else $filesize = 'dir';
+    $filedate = filemtime($filename);
+    $date = date('d-m-Y', $filedate);
+    array_push($newArray, (array($filename, $date, $filesize)));
+}
+
+echo genereTableauv2($titre, $newArray);
+echo "<br>";
+echo "<br>";
+echo"<a href='http://localhost:8888/TP2/src/tableau.php?tri=normal'>Ordre Croissant</a>";
+echo "<br>";
+echo"<a href='http://localhost:8888/TP2/src/tableau.php?tri=reverse'>Ordre Décroissant</a>";
+echo "<br>";
